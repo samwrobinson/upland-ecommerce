9cc8288b6be853676af79ba2da78bd85e76e6e25
MRVPD's MacBook Pro <MRVPDAir@MRVPDs-Air.home>
2024-10-30 13:59:47 -0400

    hard-coded redirect

--- src/functions/redirect.js
+++ src/functions/redirect.js
@@ -1,10 +1,12 @@
 exports.handler = async (event) => {
   const path = encodeURIComponent(event.path);
 
+  console.log("Redirecting to login with path:", path);  // Log the path being captured
+
   return {
     statusCode: 302,
     headers: {
-      Location: `/login/?redirect=${path}`,
+      Location: `/login/?redirect=/collection/wholesale/`,  // Pass the captured path as a query parameter
     },
   };
 };
